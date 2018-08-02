f9bb7d64e4eeb12fbcdc1c89535417b3868ee92a
Павел Любачев <lpaspb@MacBook-Pro-Pavel.local>
2018-08-02 21:42:33 +0300

    questions

--- script.js
+++ script.js
@@ -6,23 +6,97 @@ $(document).ready(function() {
     
     var charge = $(".charge");
     
+    var checks = []
+    $('.add').click(function(){
+        
+    $('.question-card-content').append('<div class="card w-85 mb-2"><div class="card-body"><h5 class="card-title">Для менеджера<span class="ml-2">✎</span></h5><p class="card-text"><span class="badge badge-light mr-2">Опыт работы <span class="m-1 text-secondary">✕</span></span><a href="#" class="btn btn-link p-0 pr-2 pl-2">+</a></p></div></div>')
+        
+    });
+        
+    $('.question_button').click(function(){
+        var option = 0;
+        option = $("#inputGroupSelectQ option:selected").text();
+        
+        if ($("#inputGroupSelectQ option:selected").val() != 0){
+            $('.draw').html('<span class="badge badge-light mr-2">' + option + '</span>');
+        } else {
+            $('.draw').html('<span class="badge badge-light mr-2">Нет</span>');
+        }
+    });
     
-     $('.check').change(function(){
+    $('#v-pills-profile-tab2').click(function(){
+        
+        $('#questions').removeClass('hide')
+        $('#vac').addClass('hide')
+        console.log('click2')
+    });
+        
+    $('#v-pills-profile-tab1').click(function(){
+        
+        $('#vac').removeClass('hide')
+        $('#questions').addClass('hide')
+        console.log('click1')
+        
+    });
+    
+       
+        
+        $(document).on('click', 'li', function(){
+        
+        if ($(this).attr("class").indexOf("it1") != -1) {
+        $('.card1').removeClass('hide')
+        $('.card2').addClass('hide')
+        $('.it1 > a').addClass('active')
+        $('.it2 > a').removeClass('active')
+            
+        } else if ($(this).attr("class").indexOf("it2") != -1) {
+            $('.card2').removeClass('hide')
+        $('.card1').addClass('hide')
+        $('.it2 > a').addClass('active')
+        $('.it1 > a').removeClass('active')
+        console.log($('.card2 < a'))
+        }
+    });
+        
+   
+        
+        
+    $('.option1').click(function(){
+         $('.draw').html('<span></span')
+        if(checks.length == 0) {
+        $('.draw').html('<span class="mr-2">нет</span>');    
+        }
+        for (var i = 0; i < checks.length; i++) {
+        
+       
+       
+        $('.draw').append('<span class="badge badge-light mr-2">' + checks[i] + '</span>');
+       
+        
+        
+        }
+    })
+        
+        
+    $('.check').change(function(){
         
          if($('.check').is(":checked")) {
             $(".manyactions").removeClass('hide');
             $(".vac-check").prop('checked', true);
              
-        } 
+             $(".ques").addClass('draw');
+          }
          else {
              
              $(".vac-check").prop('checked', false);
-             $(".manyactions").addClass('hide')
-             $(".charge").addClass('hide')
-             $(".pres").addClass('hide')
-             $(".danger").addClass('hide') 
-             
-             
+             //$(".manyactions").addClass('hide')
+             //$(".charge").addClass('hide')
+             //$(".pres").addClass('hide')
+             //$(".danger").addClass('hide') 
+             //$(".custom-select").val(0);
+
+             $(".ques").removeClass('draw');
+           
         }
          
          
@@ -33,14 +107,18 @@ $(document).ready(function() {
          if($(this).is(":checked")) {
             $(".manyactions").removeClass('hide');
             
+             $(this).parent().parent('div').parent('a').find('.ques').addClass('draw')
+             
+            
              
         } 
          else if ($(this).is(":checked") == false){
-             $(".manyactions").addClass('hide')
-             $(".charge").addClass('hide')
-             $(".pres").addClass('hide')
-             $(".danger").addClass('hide') 
+             //$(".manyactions").addClass('hide')
+             //$(".charge").addClass('hide')
+             //$(".pres").addClass('hide')
+             //$(".danger").addClass('hide') 
              
+             $(this).parent().parent('div').parent('a').find('.ques').removeClass('draw')
              
         }
          
